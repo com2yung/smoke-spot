b2331c485fd24eb8bf20cc3a6e989b70655948a6
nyangmeong <skyplye@gmail.com>
2024-05-21 23:25:05 +0900

    2

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:smoke_spot/pages/pages.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+
+
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:smoke_spot/pages/pages.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            Text('Smoke Spot',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
